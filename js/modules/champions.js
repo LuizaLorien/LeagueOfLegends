6cf9d701dd2b1b33523c0dcb621a4b1be35c4c84
LuizaLorien <luiza.l.pereira@aluno.senai.br>
2024-02-27 23:22:29 -0300

    adding fonts, cursor and header

--- js/modules/champions.js
+++ js/modules/champions.js
@@ -1,7 +1,8 @@
-    // Fetch dados dos campeões da api
-    fetch('https://ddragon.leagueoflegends.com/cdn/14.4.1/data/en_US/champion.json')
-    .then(response => response.json())
-    .then(data => {
+fetch('https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json')
+
+  .then(response => response.json())
+  .then(data => {
+
 
     // extrair os dados dos campeões da resposta da api
     const champions = data.data;
@@ -11,16 +12,22 @@
 
     //cria um item de lista para cada campeao
     for (const id in champions) {
+
       const champion = champions[id];
       const listItem = document.createElement('li');
       listItem.textContent = `${champion.id}: ${champion.name}`;
       championList.appendChild(listItem);
+
     }
 
     // adiciona pra pagina de personagem aaaaaa
     const championContainer = document.getElementById('championData');
     championContainer.appendChild(championList);
-    })
-    .catch(error => {
-        console.error('Error fetching champion data:', error);
-    });
+
+
+  })
+
+  .catch(error => {
+    console.error('Error fetching champion data:', error);
+
+  });
